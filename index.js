0aa7aed9d641407e3b18aededdc8298825b70918
Christina <christina.e.c.barberi@gmail.com>
2023-03-06 14:30:28 -0500

    added util and chalk const

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
+const util = require('util');
 const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer');
 const Intern = require('./lib/Intern');
 const generateHTML = require('./src/generateHTML');
+const chalk = require('chalk');
 
 // writeFile function
 const writeFileAsync = util.promisify(fs.writeFile);
